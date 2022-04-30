61441420bbff1d1be13238cb98a838e412d46d88
Marcin Benka <marcin.benka@gmail.com>
2022-04-30 03:41:39 +0200

    [obeny] [fun]: Make OSD more entertaining - battery and turtle

---
+++ src/main/osd/osd_text.h
@@ -0,0 +1,15 @@
+#pragma once
+
+//                                  > CRASH FLIP <
+#define OSD_WARN_CRASH_FLIP        "> TURTLE MODE <"
+//                                  CRASH FLIP SWITCH
+#define OSD_WARN_CRASH_FLIP_SWITCH "TURTLE MODE SWITCH"
+//                                  LAND NOW
+#define OSD_WARN_LAND_NOW          "IT HURTS"
+//                                  LOW BATTERY
+#define OSD_WARN_LOW_BATTERY       "DON'T CARE"
+
+//                                  ARMED
+#define OSD_TEXT_ARMED             "GO WILD"
+
+#define OSD_KACK_LEVEL             (90)
